1cf377d89caec37ca7cb3bc4fe46c51268173560
zonbimutin <38836980+zonbimutin@users.noreply.github.com>
2018-08-29 23:15:11 +0200

    Boton de info con mensaje independiente	por imagen

--- RicheOuPauvre/Controller/ViewController.swift
+++ RicheOuPauvre/Controller/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var leftButtomLabel: UIButton!
     
+    @IBOutlet weak var infoButtonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,14 +32,47 @@ class ViewController: UIViewController {
     @IBAction func rigthButtom(_ sender: UIButton) {
         
         self.imageSelected.image = UIImage(named:"violin-35026" )
+        self.imageSelected.accessibilityIdentifier = "Pauvre"
         
     }
     
     @IBAction func leftButtom(_ sender: UIButton) {
         
         self.imageSelected.image = UIImage(named:"man-160928" )
+        self.imageSelected.accessibilityIdentifier = "Riche"
         
     }
     
-}
+    @IBAction func infoButton(_ sender: UIButton) {
+        
+        var riche = """
+                                                                I am rich,
+                                                                I deserv it,
+                                                                I am good,
+                                                                healthy & successful
+                                                                """
+        var pauvre = """
+                                                                I am pauvre,
+                                                                I deserv it,
+                                                                I am good,
+                                                                healthy & successful
+                                                                """
+        var message = ""
+        
+        if self.imageSelected.accessibilityIdentifier == "Riche" {
+            message = riche
+        }
+        if self.imageSelected.accessibilityIdentifier == "Pauvre" {
+            message = pauvre
+        }
+        
+        let controller = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        
+        controller.addAction(action)
+        self.show(controller, sender: nil)
+    }
 
+
+}
